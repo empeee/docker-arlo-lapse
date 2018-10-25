9673df8b5ea17fb36a908ae2f982b214e9f9229b
Mark Pude <markpude@gmail.com>
2018-10-25 14:21:07 -0400

    changed name, added logger, added compression'

--- arlo-lapse.py
+++ arlo-lapse.py
@@ -4,19 +4,20 @@ import glob
 import re
 import os
 import imageio
-import time
 import timeout_decorator
 import yaml
+import logging
 
-DEBUG = True
-
-GIF_PATH = './gif/'
-SNAPSHOT_PATH = './raw/'
 CONFIG_PATH = './cfg/'
-PURGE_DURATION_HOURS = 24
-GIF_FRAME_SECONDS = 0.25
 
-class ArloGIF:
+LAPSE_PATH = './lapse/'
+SNAPSHOT_PATH = './raw/'
+PURGE_DURATION_HOURS = 24
+LAPSE_DURATION = 10
+
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=logging.DEBUG)
+
+class ArloLapse:
     """
     This is a class to generate animated GIFs of Arlo stills.
 
@@ -27,7 +28,7 @@ class ArloGIF:
         username (str): Arlo username
         password (str): Arlo password
         camera_names (list of strings): List of camera names to use
-        gif_path (str): Path to resulting GIFs
+        lapse_path (str): Path to resulting GIFs
         snapshot_path (str): Path to raw snapshots
         purge_duration_hours (float): Time in hours to retain images
         gif_frame_seconds (float): Frame duration in resulting GIF
@@ -35,6 +36,8 @@ class ArloGIF:
 
     def __init__(self):
         """ Constructor for ArloGIF class """
+
+        logging.info('ArloLapse.__init__(): Initializing...')
 
         with open(CONFIG_PATH + 'config.yaml', 'r') as f:
             try:
@@ -52,30 +55,41 @@ class ArloGIF:
         else:
             raise ValueError('Cannot find password in config.yaml')
 
-        if 'camera_names' in cfg_yaml:
-            self.camera_names = cfg_yaml['camera_names']
+        camera_names = cfg_yaml.get('camera_names')
+        if camera_names:
+            self.camera_names = camera_names
+            logging.debug('ArloLapse.__init__(): Found camera_names: ' + ', '.join(camera_names))
         else:
             self.camera_names = []
 
-        if 'gif_path' in cfg_yaml:
-            self.gif_path = cfg_yaml['gif_path']
+        lapse_path = cfg_yaml.get('lapse_path')
+        if lapse_path:
+            self.lapse_path = lapse_path
+            logging.debug('ArloLapse.__init__(): Found lapse_path: ' + lapse_path)
         else:
-            self.gif_path = GIF_PATH
+            self.lapse_path = LAPSE_PATH
 
-        if 'snapshot_path' in cfg_yaml:
-            self.snapshot_path = cfg_yaml['snapshot_path']
+        snapshot_path = cfg_yaml.get('snapshot_path')
+        if snapshot_path:
+            self.snapshot_path = snapshot_path
+            logging.debug('ArloLapse.__init__(): Found snapshot_path: ' + snapshot_path)
         else:
             self.snapshot_path = SNAPSHOT_PATH
 
-        if 'purge_duration_hours' in cfg_yaml:
-            self.purge_duration_hours = cfg_yaml['purge_duration_hours']
+        purge_duration_hours = cfg_yaml.get('purge_duration_hours')
+        if purge_duration_hours:
+            self.purge_duration_hours = purge_duration_hours
+            logging.debug('ArloLapse.__init__(): Found purge_duration_hours: ' + purge_duration_hours)
         else:
             self.purge_duration_hours = PURGE_DURATION_HOURS
 
-        if 'gif_frame_seconds' in cfg_yaml:
-            self.gif_frame_seconds = cfg_yaml['gif_frame_seconds']
+        lapse_duration = cfg_yaml.get('lapse_duration')
+        if lapse_duration:
+            self.lapse_duration = lapse_duration
+            logging.debug('ArloLapse.__init__(): Found lapse_duration: ' + lapse_duration)
         else:
-            self.gif_frame_seconds = GIF_FRAME_SECONDS
+            self.lapse_duration = LAPSE_DURATION
+
 
     @timeout_decorator.timeout(60)
     def get_snapshot_url(self, arlo, basestation, camera):
@@ -101,7 +115,7 @@ class ArloGIF:
         If the camera list wasn't given, get all cameras from Arlo
         """
 
-        if DEBUG: print('\nGetting Snapshots')
+        logging.info('ArloLapse.get_snapshots(): Getting snapshots...')
 
         try:
             arlo = Arlo(self.username, self.password)
@@ -118,24 +132,24 @@ class ArloGIF:
 
 
             if not self.camera_names:
-                if DEBUG: print('  No camera names given, getting from Arlo')
+                logging.debug('ArloLapse.get_snapshots(): No camera names given, getting from Arlo')
                 self.camera_names = camera_names
             else:
-                if DEBUG: print('  Checking if given camera names are in Arlo')
+                logging.debug('ArloLapse.get_snapshots(): Checking if given camera names are in Arlo')
                 self.camera_names = list(set(self.camera_names) & set(camera_names))
 
-            if DEBUG: print('    Final list of cameras: ' + ', '.join(self.camera_names))
+            logging.debug('ArloLapse.get_snapshots(): Final list of cameras: ' + ', '.join(self.camera_names))
 
             for camera in cameras:
                 camera_name = camera['deviceName'].replace(' ', '_')
                 if camera_name in self.camera_names:
-                    if DEBUG: print('  Getting snapshot for ' + camera_name)
+                    logging.debug('ArloLapse.get_snapshots(): Getting snapshot for ' + camera_name)
                     snapshot_file = self.snapshot_path + camera_name + '_' + now_str + '.jpg'
                     try:
                         snapshot_url = self.get_snapshot_url(arlo, basestations[0], camera)
                         arlo.DownloadSnapshot(snapshot_url,snapshot_file)
                     except timeout_decorator.TimeoutError:
-                        if DEBUG: print('    Timeout ' + camera_name)
+                        logging.warning('ArloLapse.get_snapshots(): Timeout ' + camera_name)
 
         except Exception as e:
             print(e)
@@ -147,7 +161,7 @@ class ArloGIF:
         Pulls age of snapshot from filename.
         """
 
-        if DEBUG: print('\nPurging Snapshots')
+        logging.info('ArloLapse.purge_snapshots(): Purging Snapshots...')
         now = datetime.datetime.now()
 
         for camera_name in self.camera_names:
@@ -159,30 +173,36 @@ class ArloGIF:
                 match = re.search(regex, file)
                 date = datetime.datetime.strptime(match.group(2), '%Y%m%d%H%M%S')
                 if date < now - datetime.timedelta(hours=self.purge_duration_hours):
-                    if DEBUG: print('  Purging ' + file)
+                    logging.debug('ArloLapse.purge_snapshots(): Purging ' + file)
                     os.remove(file)
 
-    def make_gifs(self):
+    def make_lapse(self):
         """ Method to generate GIF from available snapshots. """
 
-        if DEBUG: print('\nMaking GIFs')
+        logging.info('ArloLapse.make_lapse(): Making Time Lapses...')
+
         for camera_name in self.camera_names:
             files = sorted(glob.glob(self.snapshot_path + camera_name + '*.jpg'))
             num_files = len(files)
-            if DEBUG: print('  Found ' + str(num_files) + ' images for ' + camera_name)
+            logging.debug('ArloLapse.make_lapse(): Found ' + str(num_files) + ' images for ' + camera_name)
 
             if num_files>0:
+                fps = num_files/self.lapse_duration
                 images = []
                 for file in files:
                     images.append(imageio.imread(file))
-                output_file = self.gif_path + camera_name + '.gif'
-                imageio.mimsave(output_file, images, duration=self.gif_frame_seconds, subrectangles=True)
+                output_file = self.snapshot_path + camera_name + '.gif'
+                final_file = self.lapse_path + camera_name + 'gif'
+                imageio.mimwrite(output_file, images, fps=fps)
+
+                command = 'gifsicle -O3 --colors 128 --resize-width 512 {} > {}'.format(output_file, final_file)
+                os.system(command)
 
 
 
 if __name__ =='__main__':
-    arlo_gif = ArloGIF()
+    arlo_gif = ArloLapse()
     arlo_gif.get_snapshots()
     arlo_gif.purge_snapshots()
-    arlo_gif.make_gifs()
+    arlo_gif.make_lapse()
 
